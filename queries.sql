aa051a27f70ccb950b6dc281f965245ba7b1f47b
Jonathan Lui <jonslui@gmail.com>
2020-06-03 13:12:06 -0400

    9.1 - 9.9

--- queries.sql
+++ queries.sql
@@ -492,3 +492,63 @@ SELECT name,
        ELSE 'None'
   END
 FROM teacher;
+
+-- question 9.1
+SELECT COUNT(id)
+  FROM stops;
+
+-- question 9.2
+SELECT id
+  FROM stops
+  WHERE name = 'Craiglockhart';
+
+-- question 9.3
+SELECT stops.id, stops.name
+  FROM stops RIGHT JOIN route ON route.stop = stops.id
+  WHERE route.num = 4 AND route.company = 'LRT';
+
+-- question 9.4
+SELECT company, num, COUNT(*)
+  FROM route WHERE stop=149 OR stop=53
+  GROUP BY company, num
+  HAVING COUNT(*) = 2;
+
+-- question 9.5
+
+
+-- question 9.6 List all services which connect Craiglockhart with London Road
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart'
+AND stopb.name = 'London Road'
+GROUP BY num;
+
+-- question 9.7 Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+SELECT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Haymarket'
+AND stopb.name = 'Leith'
+GROUP BY num;
+
+-- question 9.8 Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+SELECT a.company, a.num
+FROM route a JOIN route b ON (a.company = b.company AND a.num = b.num)
+             JOIN stops stopa ON (a.stop = stopa.id)
+             JOIN stops stopb ON (b.stop = stopb.id)
+WHERE stopa.name = 'Craiglockhart' 
+  AND stopb.name = 'Tollcross'
+GROUP BY num;
+
+-- question 9.9 Distinct stops that can be reached from 'Craiglockhart' by taking one bus
+SELECT stopb.name, a.company, a.num
+FROM route a JOIN route b ON
+(a.company = b.company AND a.num = b.num)
+JOIN stops stopa ON  (a.stop = stopa.id)
+JOIN stops stopb ON (b.stop = stopb.id)
+WHERE stopa.name = 'Craiglockhart' AND a.company = 'LRT'
